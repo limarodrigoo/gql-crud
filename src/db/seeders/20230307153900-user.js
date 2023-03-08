cf18180de46ecd40777eecfede61cec57fffde5a
Rodrigo Lima <limarodrigoo@outlook.com>
2023-03-08 13:47:26 -0300

    create first seeders

--- src/db/seeders/20230307153900-user.js
+++ src/db/seeders/20230307153900-user.js
@@ -1,18 +1,45 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-   return queryInterface.bulkInsert('Users', [{
-      firstName: 'Rodrigo',
-      lastName: 'Lima',
-      email: 'rodrigo@teste.com',
-      createdAt: new Date(),
-      updatedAt: new Date()
-   }])
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert("Users", [
+      {
+        firstName: "Rodrigo",
+        lastName: "Lima",
+        email: "rodrigo@teste.com",
+        hourlyRate: 32.1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        firstName: "Gabriel",
+        lastName: "lobo",
+        email: "lobo@teste.com",
+        hourlyRate: 42.1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        firstName: "Emanuele",
+        lastName: "Silva",
+        email: "emanuelle@teste.com",
+        hourlyRate: 22.1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        firstName: "Lucas",
+        lastName: "texugo",
+        email: "lucas@teste.com",
+        hourlyRate: 52.1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
-   return queryInterface.bulkDelete('Users', null, {})
-  }
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete("Users", null, {});
+  },
 };
